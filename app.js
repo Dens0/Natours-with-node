80de4b82b0502d2869449503f53de23ab63e2a37
Dens0 <adrian.sutkowski@gmail.com>
2020-07-27 15:35:30 +0200

    added some middleware's and user add methods

--- app.js
+++ app.js
@@ -1,27 +1,36 @@
 const fs = require('fs');
 
 const express = require('express');
-
+const morgan = require('morgan');
 const app = express();
 
+
+//1). Middleweres
+app.use(morgan('env'));
 app.use(express.json()); //middleware between request and response
+//taka jest konwencja
+app.use((req, res, next) => {
+  // trzeba tutaj przywołąć next ponieważ bez tego  cykl req res się zablokuje i nie zwróci odpowiedzi nigdy
+  console.log('Hello from the middleware');
+  next();
+
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+
+  next();
+
+});
 
 
-// app.get('/',(req,res)=>{
-//   res.status(200).json({ message:'Hello from the server side!',app:'Natours' })
-//
-// })
-//
-// app.post('/',(req,res)=>{
-//   res.send('You can post to this endpoint')
-// } )
-
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
-
-app.get('/api/v1/tours', (req, res) => {
-
+tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+//Routes handlers
+const getAllTours = (req, res) => {
+  console.log(req.requestTime);
   res.status(200).json({
     status: 'success',
+    requestedAt: req.requestTime,
     results: tours.length,
     data: {
       tours
@@ -30,9 +39,35 @@ app.get('/api/v1/tours', (req, res) => {
   });
 
 
-});
+};
+const getTour = (req, res) => {
 
-app.post('/api/v1/tours', (req, res) => {
+  console.log(req.params);
+
+  const id = req.params.id * 1;
+  const tour = tours.find(el => el.id === id);
+
+  // if (id>tours.length){
+
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'INVALID ID'
+    });
+  }
+
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour
+
+    }
+  });
+
+
+};
+const addTour = (req, res) => {
 // console.log(req.body);
 
 
@@ -49,9 +84,123 @@ app.post('/api/v1/tours', (req, res) => {
     });
 
   });
-});
+};
+const updateTour = (req, res) => {
 
+
+  // if (id>tours.length){
+
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'INVALID ID'
+    });
+  }
+  res.status(200).json({
+    status: 'success',
+    data: null
+  });
+};
+const deleteTour = (req, res) => {
+
+
+  // if (id>tours.length){
+
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'INVALID ID'
+    });
+  }
+  res.status(204).json({
+    status: 'success',
+    data: {
+      tour: '<Updated tour here...>'
+    }
+  });
+};
+
+const getAllUsers = (req, res) => {
+res.status(500).json({
+  status: 'error',
+  message: 'This route is not yet defined'
+})
+
+
+};
+const addUser = (req, res) => {
+
+
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined'})
+};
+const getUser = (req, res) => {
+
+
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined'})
+};
+const updateUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined'})
+};
+const deleteUser = (req, res) => {
+
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined'})
+};
+
+
+
+
+// app.get('/api/v1/tours', getAllTours);
+// app.post('/api/v1/tours', addTour);
+// app.get('/api/v1/tours/:id', getTour);
+// app.patch('/api/v1/tours/:id', updateTour);
+// app.delete('/api/v1/tours/:id', deleteTour);
+
+
+//ROUTES
+
+app.route('/api/v1/tours')
+  .get(getAllTours)
+  .post(addTour);
+
+
+app
+  .route('/api/v1/tours/:id')
+  .get(getTour)
+  .patch(updateTour)
+  .delete(deleteTour);
+
+app
+  .route('/api/v1/users')
+  .get(getAllUsers)
+  .post(addUser);
+
+app
+  .route('/api/v1/users/:id')
+  .get(getUser)
+  .patch(updateUser)
+  .delete(deleteUser);
+
+
+//START THE SERVER
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
+
+
+// app.get('/',(req,res)=>{
+//   res.status(200).json({ message:'Hello from the server side!',app:'Natours' })
+//
+// })
+//
+// app.post('/',(req,res)=>{
+//   res.send('You can post to this endpoint')
+// } )
